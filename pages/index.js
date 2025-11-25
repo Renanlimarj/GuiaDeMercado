c9be6fb1bdb2a371860c2014190cf4700e171993
Renanlimarj <31229314+Renanlimarj@users.noreply.github.com>
2025-11-25 08:15:06 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
-    return <h1>Marina, eu te amo!! <br><br>
+    return <h1>Marina, eu te amo!!</h1>
 
-Estou fazendo um cursinho e tinha um desafio de fazer um deploy pra impactar alguém 😂</h1>
+<h2>Estou fazendo um cursinho e tinha um desafio de fazer um deploy pra impactar alguém 😂</h2>
 }
 
 
