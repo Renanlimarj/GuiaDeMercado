867e4d45455030a38b9424d16497159899debde0
Renanlimarj <31229314+Renanlimarj@users.noreply.github.com>
2025-11-28 04:50:03 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function Home() {
         fontFamily: "Arial, sans-serif",
       }}
     >
-      <h1>Em construção</h1>
+      <h1>Em construção 🚧</h1>
 
       <img
         src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200"
