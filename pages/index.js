c44de25cf5e5ebea1fa474807441abfe955ea312
Renanlimarj <31229314+Renanlimarj@users.noreply.github.com>
2025-11-28 04:52:59 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-function Home() {
+// pages/index.js
+export default function Home() {
   return (
     <div
       style={{
+        display: "flex",
+        alignItems: "center",      // centraliza verticalmente
+        justifyContent: "center",  // centraliza horizontalmente
+        minHeight: "100vh",        // altura full viewport
         textAlign: "center",
         padding: "40px",
         fontFamily: "Arial, sans-serif",
       }}
     >
       <h1>Em construção 🚧</h1>
-
-      
     </div>
   );
 }
-
-export default Home;
